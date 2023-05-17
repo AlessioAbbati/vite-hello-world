d37b171d91f621fb1f5a512ff7f78d3fcce4d2d4
AlessioAbbati <alex_117@live.it>
2023-05-17 17:17:28 +0200

    add first component

---
+++ src/components/titleComponent.vue
@@ -0,0 +1,12 @@
+<script>
+   
+</script>
+
+<template>
+   <h1>La mia app con vite!</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit odit commodi quisquam nulla corrupti, consectetur, voluptatum tempore quam libero praesentium ipsam omnis minima eligendi temporibus quos, eius optio dolores similique.</p>
+</template>
+
+<style scoped>
+
+</style>
