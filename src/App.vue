5e4c712252951c136118d35cd41c6ebd2d66c759
AlessioAbbati <alex_117@live.it>
2023-05-17 17:56:48 +0200

    add second component (bonus)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script>
   import titleComponent from "./components/titleComponent.vue"
+  import AppHero from "./components/AppHero.vue"
   export default {
     components: {
       titleComponent,
+      AppHero,
     }
   }
 </script>
 
 <template>
   <titleComponent />
+  <AppHero />
+  <h1>io sono il principale</h1>
 </template>
 
 <style scoped>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,13 @@
+<script>
+  
+</script>
+
+<template>
+  <div>
+    <img src="https://render.fineartamerica.com/images/rendered/default/print/6.250/8.000/break/images/artworkimages/medium/1/zoidberg-jesus-katie-clark.jpg" alt="">
+  </div>
+</template>
+
+<style scoped>
+  
+</style>
